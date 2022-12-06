29cf7b3cb2426add3e290676ed2b4a121feb83dc
Mohamed Ibrahim <109217422+memo-ibrahim-alean@users.noreply.github.com>
2022-12-06 10:40:45 +0200

    The renderCharacter() function

--- index.js
+++ index.js
@@ -1,19 +1,26 @@
-const heroDiv = document.getElementById("hero");
-const monsterDiv = document.getElementById("monster");
+const heroElementId = "hero";
+const heroName = "Wizard";
+const heroAvatar = "images/wizard.png";
+const heroHealth = "60";
+const heroDiceRoll = 6;
 
-heroDiv.innerHTML = `
-   <div class="character-card">
-      <h4 class="name">Wizard</h4>
-      <img class="avatar" src="images/wizard.png" />
-      <p class="health">health: <b> 60 </b></p>
-      <div class="dice-container"><div class="dice">6</div></div>
-   </div>
-`;
-monsterDiv.innerHTML = `
-   <div class="character-card">
-      <h4 class="name">Orc</h4>
-      <img class="avatar" src="images/orc.png" />
-      <p class="health">health: <b> 10 </b></p>
-      <div class="dice-container"><div class="dice">4</div></div>
-   </div>
-`;
+const monsterElementId = "monster";
+const monsterName = "Orc";
+const monsterAvatar = "images/orc.png";
+const monsterHealth = "10";
+const monsterDiceRoll = 4;
+
+function renderCharacter(elementId, name, avatar, health, diceRoll) {
+  document.getElementById(elementId).innerHTML = `
+  <div class="character-card">
+      <h4 class="name"> ${name} </h4>
+      <img class="avatar" src="${avatar}"/>
+      <p class="health">health: <b> ${health}</b></p>
+      <div class="dice-container">
+         <div class="dice"> ${diceRoll} </div>
+      </div>
+   </div>`;
+}
+
+renderCharacter(heroElementId, heroName, heroAvatar, heroHealth, heroDiceRoll);
+renderCharacter(monsterElementId, monsterName, monsterAvatar, monsterHealth, monsterDiceRoll);
