0af3d56eaab13979207a867ee5d6b5c4dcb14cde
Mohamed Ibrahim <109217422+memo-ibrahim-alean@users.noreply.github.com>
2022-12-06 10:27:00 +0200

    Render the characters

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
-/*
-Challenge:
-1. Take the hard-coded HTML for the Wizard card, bring it
-   into index.js and then inject it back into its div with
-   JavaScript.
-2. Do the same for Orc card.
-*/
+const heroDiv = document.getElementById("hero");
+const monsterDiv = document.getElementById("monster");
+
+heroDiv.innerHTML = `
+   <div class="character-card">
+      <h4 class="name">Wizard</h4>
+      <img class="avatar" src="images/wizard.png" />
+      <p class="health">health: <b> 60 </b></p>
+      <div class="dice-container"><div class="dice">6</div></div>
+   </div>
+`;
+monsterDiv.innerHTML = `
+   <div class="character-card">
+      <h4 class="name">Orc</h4>
+      <img class="avatar" src="images/orc.png" />
+      <p class="health">health: <b> 10 </b></p>
+      <div class="dice-container"><div class="dice">4</div></div>
+   </div>
+`;
