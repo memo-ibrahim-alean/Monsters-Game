6ba6d98f0b803b19275210ab588520da1ef05cc7
Mohamed Ibrahim <109217422+memo-ibrahim-alean@users.noreply.github.com>
2022-12-12 16:47:48 +0200

    Object Destructuring in renderCharacter()

--- index.js
+++ index.js
@@ -15,13 +15,14 @@ const monster = {
 };
 
 function renderCharacter(data) {
-  document.getElementById(data.elementId).innerHTML = `
+  const { elementId, name, avatar, health, diceRoll } = data;
+  document.getElementById(elementId).innerHTML = `
   <div class="character-card">
-      <h4 class="name"> ${data.name} </h4>
-      <img class="avatar" src="${data.avatar}" />
-      <div class="health">health: <b> ${data.health} </b></div>
+      <h4 class="name"> ${name} </h4>
+      <img class="avatar" src="${avatar}" />
+      <div class="health">health: <b> ${health} </b></div>
       <div class="dice-container">
-            <div class="dice"> ${data.diceRoll} </div>
+            <div class="dice"> ${diceRoll} </div>
       </div>
    </div>
    `;
