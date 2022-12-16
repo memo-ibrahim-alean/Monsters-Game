e58540d4cf63378a15e00abbcddea6a5a49479b7
Mohamed Ibrahim <109217422+memo-ibrahim-alean@users.noreply.github.com>
2022-12-16 10:47:41 +0200

    Export getDiceRollArray()

--- index.js
+++ index.js
@@ -1,19 +1,5 @@
-const characterData = {
-  hero: {
-    elementId: "hero",
-    name: "Wizard",
-    avatar: "images/wizard.png",
-    health: 60,
-    diceCount: 3,
-  },
-  monster: {
-    elementId: "monster",
-    name: "Orc",
-    avatar: "images/orc.png",
-    health: 10,
-    diceCount: 1,
-  }
-};
+import characterData from "./data.js";
+import getDiceRollArray from "./utils.js";
 
 function Character(data) {
   Object.assign(this, data);
@@ -42,19 +28,12 @@ function Character(data) {
   };
 }
 
-function getDiceRollArray(diceCount) {
-  return new Array(diceCount).fill(0).map(function () {
-    return Math.floor(Math.random() * 6) + 1;
-  });
-}
-
-const wizard = new Character(characterData.hero);
-const orc = new Character(characterData.monster);
-
 function render() {
   document.getElementById(wizard.elementId).innerHTML =
     wizard.getCharacterHtml();
   document.getElementById(orc.elementId).innerHTML = orc.getCharacterHtml();
 }
 
+const wizard = new Character(characterData.hero);
+const orc = new Character(characterData.monster);
 render();
