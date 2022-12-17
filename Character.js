30494c25ae2ccbb327e4104e788f50d8e0ed6956
Mohamed Ibrahim <109217422+memo-ibrahim-alean@users.noreply.github.com>
2022-12-17 10:35:50 +0200

    Render placeholder dice

--- Character.js
+++ Character.js
@@ -1,7 +1,8 @@
-import { getDiceRollArray } from "./utils.js";
+import { getDiceRollArray, getDicePlaceholderHtml } from "./utils.js";
 
 function Character(data) {
   Object.assign(this, data);
+  this.diceArray = getDicePlaceholderHtml(this.diceCount);
 
   this.getDiceHtml = function (diceCount) {
     return getDiceRollArray(diceCount)
@@ -12,16 +13,17 @@ function Character(data) {
   };
 
   this.getCharacterHtml = function () {
-    const { elementId, name, avatar, health, diceCount } = this;
+    const { name, avatar, health, diceCount } = this;
+
     let diceHtml = this.getDiceHtml(diceCount);
     return `
     <div class="character-card">
-        <h4 class="name"> ${name} </h4>
-        <img class="avatar" src="${avatar}" />
-        <div class="health">health: <b> ${health} </b></div>
-        <div class="dice-container">
-              ${diceHtml}
-        </div>
+    <h4 class="name"> ${name} </h4>
+    <img class="avatar" src="${avatar}" />
+    <div class="health">health: <b> ${health} </b></div>
+    <div class="dice-container">
+    ${this.diceArray}
+    </div>
     </div>
     `;
   };
--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 import characterData from "./data.js";
-import Character from "./Character.js"
+import Character from "./Character.js";
 
-function render() {
-  document.getElementById(wizard.elementId).innerHTML =
-    wizard.getCharacterHtml();
-  document.getElementById(orc.elementId).innerHTML = orc.getCharacterHtml();
+function attack() {
+  render();
 }
 
+function render() {
+  document.getElementById("hero").innerHTML = wizard.getCharacterHtml();
+  document.getElementById("monster").innerHTML = orc.getCharacterHtml();
+}
+
+document.getElementById("attack-button").addEventListener("click", attack);
 const wizard = new Character(characterData.hero);
 const orc = new Character(characterData.monster);
+
 render();
