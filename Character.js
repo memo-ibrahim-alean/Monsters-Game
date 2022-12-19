055402026ddcb71158289094584c8810579ce893
Mohamed Ibrahim <109217422+memo-ibrahim-alean@users.noreply.github.com>
2022-12-19 11:46:33 +0200

    Calculate percentage health remaining

--- Character.js
+++ Character.js
@@ -1,23 +1,31 @@
 import { getDiceRollArray, getDicePlaceholderHtml } from "./utils.js";
 
+const getPercentage = (remainingHealth, maximumHealth) => (100 * remainingHealth) / maximumHealth;
+
 function Character(data) {
   Object.assign(this, data);
+
+  this.maxHealth = this.health;
   this.diceArray = getDicePlaceholderHtml(this.diceCount);
 
   this.takeDamage = function (attackScoreArray) {
-    const totalAttackScore = attackScoreArray.reduce((total, currentEle) => total + currentEle);
+    const totalAttackScore = attackScoreArray.reduce(
+      (total, currentEle) => total + currentEle
+    );
     this.health -= totalAttackScore;
     if (this.health <= 0) {
       this.dead = true;
       this.health = 0;
     }
 
+    console.log(getPercentage(this.health, this.maxHealth));
   };
 
   this.getDiceHtml = function () {
     this.currentDiceScore = getDiceRollArray(this.diceCount);
     this.diceArray = this.currentDiceScore
-      .map(num => `<div class="dice">${num}</div>`).join("");
+      .map((num) => `<div class="dice">${num}</div>`)
+      .join("");
   };
 
   this.getCharacterHtml = function () {
